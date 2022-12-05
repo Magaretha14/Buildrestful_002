86e15fb1b60872a980beb505d38bca254c50a7c9
Magaretha14 <magaretha.wahyunda@gmail.com>
2022-12-05 09:54:53 +0700

    first commit

---
+++ src/main/java/ws/a/praktikumpws/PraktikumpwsApplication.java
@@ -0,0 +1,13 @@
+package ws.a.praktikumpws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraktikumpwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraktikumpwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ws/a/praktikumpws/PraktikumpwsApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.a.praktikumpws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraktikumpwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
